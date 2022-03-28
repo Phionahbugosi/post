54ac729e7cc4003931ccb328589ed14672b2e7dc
Phionahbugosi <phionahbugosi@gmail.com>
2022-03-28 12:55:15 +0300

    javascript assignment

---
+++ Desktop/jsAssignment/condition.js
@@ -0,0 +1,14 @@
+
+
+
+let temperature=50;
+if(temperature>=20 && temperature<=36){
+    console.log("normal body temperature");
+}
+else if(temperature>=37 && temperature<=40){
+    console.log("temperature above normal");
+
+}
+else{
+    console.log("temperatures very high");
+}
